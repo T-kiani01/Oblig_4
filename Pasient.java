1dda657cea8497505c6f60fa74108668e42ef7d5
F-Zan97 <128481496+F-Zan97@users.noreply.github.com>
2023-03-21 14:32:31 +0100

    Add files via upload

---
+++ Pasient.java
@@ -0,0 +1,41 @@
+public class Pasient{
+    String navn;
+    String fodselnummer;
+    int pasientID;
+    static int nestePasientID = 1;
+    Stabel <Resept> resepter;
+
+
+    public Pasient (String navn, String fodselnummer){
+        this.navn = navn;
+        this.fodselnummer= fodselnummer;
+        this.resepter = new Stabel<>();
+        pasientID= nestePasientID;
+        nestePasientID++;
+    }
+
+    public String hentNavn() {
+        return navn;
+    }
+
+    public String hentFødselsnummer() {
+        return fodselnummer;
+    }
+
+    public int hentPasientID() {
+        return pasientID;
+    }
+
+    public void leggTilResept(Resept resept) {
+        resepter.leggTil(resept);
+    }
+
+    public Lenkeliste<Resept> hentResepter() {
+        return resepter;
+    }
+
+
+    
+
+
+}
