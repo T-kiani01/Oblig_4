073480fa0975fcf4f533929236c40e48b19290d4
T-kiani01 <104518777+T-kiani01@users.noreply.github.com>
2023-03-24 00:17:02 +0100

    Add files via upload
    E1 legesystem er blitt debugga, ennå ikke helt ferdig.

--- Legesystem.java
+++ Legesystem.java
@@ -4,129 +4,124 @@ import java.util.Scanner;
 
 public class Legesystem {
 
-    public Indeksertliste pasienter;
-    public Indeksertliste leger;
-    public Indeksertliste legemidler;
-    public Indeksertliste resepter;
+    public IndeksertListe pasienter;
+    public IndeksertListe leger;
+    public IndeksertListe legemidler;
+    public IndeksertListe resepter;
 
     public Legesystem() {
-        pasienter = new Indeksertliste<Pasient>();
-        leger = new Indeksertliste<Lege>();
-        legemidler = new Indeksertliste<Legemiddel>();
-        resepter = new Indeksertliste<Resept>();
+        pasienter = new IndeksertListe<Pasient>();
+        leger = new IndeksertListe<Lege>();
+        legemidler = new IndeksertListe<Legemiddel>();
+        resepter = new IndeksertListe<Resept>();
     }
 
+    public void lesFraFil(String filnavn) {
+        try {
+            Scanner fil = new Scanner(new File(filnavn));
+            String linje = fil.nextLine();
 
-   
+            // Les inn pasienter
+            if (linje.equals("# Pasienter")) {
+                while (fil.hasNextLine()) {
+                    linje = fil.nextLine();
+                    if (linje.startsWith("#")) {
+                        break;
+                    }
+                    String[] pasientData = linje.split(",");
+                    String navn = pasientData[0];
+                    String fnr = pasientData[1];
+                    Pasient pasient = new Pasient(navn, fnr);
+                    pasienter.leggTil(pasient);
+                }
+            }
 
+            // Les inn legemidler
+            if (linje.equals("# Legemidler")) {
+                while (fil.hasNextLine()) {
+                    linje = fil.nextLine();
+                    if (linje.startsWith("#")) {
+                        break;
+                    }
+                    String[] legemiddelData = linje.split(",");
+                    String navn = legemiddelData[0];
+                    String type = legemiddelData[1];
+                    int pris = Integer.parseInt(legemiddelData[2]);
+                    double virkestoff = Double.parseDouble(legemiddelData[3]);
+                    
+                    if (type.equals("vanlig")) {
+                        Legemiddel legemiddel = new Vanlig(navn, pris, virkestoff);
+                        legemidler.leggTil(legemiddel);
+                    } else if (type.equals("narkotisk")) {
+                        int styrke = Integer.parseInt(legemiddelData[4]);
+                        Legemiddel legemiddel = new Narkotisk(navn, pris, virkestoff, styrke);
+                        legemidler.leggTil(legemiddel);
+                    } else if (type.equals("vanedannende")) {
+                        int styrke = Integer.parseInt(legemiddelData[4]);
+                        Legemiddel legemiddel = new Vanedannende(navn, pris, virkestoff, styrke);
+                        legemidler.leggTil(legemiddel);
+                    }
+                }
+            }
 
-                public void lesFraFil(String filnavn) {
-                   
-                        Scanner fil = new Scanner(new File(filnavn));
-                        String linje = fil.nextLine();
+            // Les inn leger
+            if (linje.equals("# Leger")) {
+                while (fil.hasNextLine()) {
+                    linje = fil.nextLine();
+                    if (linje.startsWith("#")) {
+                        break;
+                    }
+                    String[] legeData = linje.split(",");
+                    String navn = legeData[0];
+                    String kontrollnr = legeData[1];
+                    int kontrollIDsjekk = Integer.parseInt(legeData[1]);
+                    if (kontrollIDsjekk == 0) {
+                        Lege lege = new Lege(navn);
+                        leger.leggTil(lege);
+                    } else {
+                        Spesialist spesialist = new Spesialist(navn, kontrollnr);
+                        leger.leggTil(spesialist);
+                    }
+                    }
+                    }
+                    
+                    // Les inn resepter
+            if (linje.equals("# Resepter")) {
+                while (fil.hasNextLine()) {
+                    linje = fil.nextLine();
+                    if (linje.startsWith("#")) {
+                        break;
+                }
+                String[] reseptData = linje.split(",");
+                int legemiddelNummer = Integer.parseInt(reseptData[0]);
+
+                //lager nytt legeobjekt
+                Lege legeNavn = new Lege(reseptData[1]);
                 
-                        // Les inn pasienter
-                        if (linje.equals("# Pasienter")) {
-                            while (fil.hasNextLine()) {
-                                linje = fil.nextLine();
-                                if (linje.startsWith("#")) {
-                                    break;
-                                }
-                                String[] pasientData = linje.split(",");
-                                String navn = pasientData[0];
-                                String fnr = pasientData[1];
-                                Pasient pasient = new Pasient(navn, fnr);
-                                pasienter.leggTil(pasient);
-                            }
-                        }
-                
-                        // Les inn legemidler
-                        if (linje.equals("# Legemidler")) {
-                            while (fil.hasNextLine()) {
-                                linje = fil.nextLine();
-                                if (linje.startsWith("#")) {
-                                    break;
-                                }
-                                String[] legemiddelData = linje.split(",");
-                                String navn = legemiddelData[0];
-                                String type = legemiddelData[1];
-                                double pris = Double.parseDouble(legemiddelData[2]);
-                                double virkestoff = Double.parseDouble(legemiddelData[3]);
-                                if (type.equals("vanlig")) {
-                                    Legemiddel legemiddel = new Vanlig(navn, pris, virkestoff);
-                                    legemidler.leggTil(legemiddel);
-                                }
-                                else if (type.equals("narkotisk")) {
-                                    int styrke = Integer.parseInt(ord[5]);
-                                    Legemiddel legemiddel = new Narkotisk(navn, pris, virkestoff, styrke);
-                                    legemidler.leggTil(legemiddel);
-                                }
-                                else if (type.equals("vanedannende")) {
-                                    int styrke = Integer.parseInt(ord[5]);
-                                    Legemiddel legemiddel = new Vanedannende(navn, pris, virkestoff, styrke);
-                                    legemidler.leggTil(legemiddel);
-                                }
-                            
-                            }
-                        }
-                        if (linje.equals("# Leger")) {
-                            while (fil.hasNextLine()) {
-                                linje = fil.nextLine();
-                                if (linje.startsWith("#")) {
-                                    break;
-                                }
-                                String[] LegeData = linje.split(",");
-                                String navn = LegeData[0];
-                                int KontrollID = Integer.parseInt(LegeData[1]);
-                                if (KontrollID == 0)
-                                {Lege lege = new Lege(navn);
-                                    leger.leggTil(lege);}
-                                else{
-                                    Spesialist spesialist = new Spesialist(navn, KontrollID);
-                                    leger.leggTil(spesialist);
-                                }
-                                
-                                
-                            }
-                        }
-                        if (linje.equals("# Resepter")) {
-                            while (fil.hasNextLine()) {
-                                linje = fil.nextLine();
-                                if (linje.startsWith("#")) {
-                                    break;
-                                }
-                                //(legemiddelNummer,legeNavn,pasientID,type,[reit])
-                                String[] ReseptData = linje.split(",");
-                                int legemiddelNummer = Integer.parseInt(ReseptData[0]);
-                                String legeNavn = ReseptData[1];
-                                String pasientID = ReseptData[2];
-                                String type = ReseptData[3];
-                                if (type.equals("blaa")) {
-                                    int reit = Integer.parseInt(ReseptData[4]);
-                                    Resept blaresept = new BlaaResepter(legemiddelNummer,legeNavn, pasientID, reit);
-                                    resepter.leggTil(blaresept);
-                                }
-                                else if (type.equals("p")) {
-                                    int reit = Integer.parseInt(ReseptData[4]);
-                                    Resept presept = new BlaaResepter(legemiddelNummer,legeNavn, pasientID, reit);
-                                    resepter.leggTil(presept);
-                                }
-                                else if (type.equals("militaer")) {
-                                    Resept militaeresept = new BlaaResepter(legemiddelNummer,legeNavn, pasientIDS);
-                                    resepter.leggTil(militaerresept);
-                                }
-                            
-                            }
-                        }
 
-                        return leger;
-                        return resepter;
-                        return pasienter;
-                        return legemidler;
+                int pasientID = Integer.parseInt(reseptData[2]);
+                String type = reseptData[3];
+                if (type.equals("blaa")) {
+                    int reit = Integer.parseInt(reseptData[4]);
+                    Resept blaaResept = new BlaaResept(null, legeNavn, pasientID, reit);
+                    resepter.leggTil(blaaResept);
+                } else if (type.equals("p")) {
+                    int reit = Integer.parseInt(reseptData[4]);
+                    Resept pResept = new PResept(null, legeNavn, pasientID, reit);
+                    resepter.leggTil(pResept);
+                } else if (type.equals("militaer")) {
+                    Resept militaerResept = new MilResept(null, legeNavn, pasientID);
+                    resepter.leggTil(militaerResept);
+                }
+            }
+        }
 
-                        
-                        
-                        }}
+        fil.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("Finner ikke fil: " + filnavn);
+    }
+}
+}
 
-
-                
+                    
+                    
